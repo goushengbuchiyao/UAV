21fdc2f0481d5ec583ebadb86cb71653ecf9e66a
goushengbuchiyao <76202143+goushengbuchiyao@users.noreply.github.com>
2025-09-12 17:50:50 +0800

    Update aruco_detection_nested.cpp
    aruco_pose_send

--- src/aruco_detection/src/aruco_detection_nested.cpp
+++ src/aruco_detection/src/aruco_detection_nested.cpp
@@ -6,7 +6,7 @@
 #include <opencv2/aruco.hpp>
 #include <eigen3/Eigen/Dense>
 #include <geometry_msgs/PoseStamped.h>
-#include <px_uav_msgs/TargetsInFrame.h>
+#include <uav_msgs/TargetsInFrame.h>
 #include <mavros_msgs/State.h>
 #include <yaml-cpp/yaml.h>
 #include <ros/package.h>
@@ -60,7 +60,7 @@ public:
         // 发布检测结果图像话题
         image_pub_ = it_.advertise("/ar_qr_detection/result", 1);
         // 初始化机体系坐标发布器
-        aruco_land_pub_ = nh_.advertise<px_uav_msgs::TargetsInFrame>("/ar_qr_detection/aruco_land_pose", 1); 
+        aruco_land_pub_ = nh_.advertise<uav_msgs::TargetsInFrame>("/ar_qr_detection/aruco_land_pose", 1); 
         // 获取 ArUco 字典
         dictionary_ = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_1000);
 
@@ -142,13 +142,14 @@ public:
 
             if (ids.empty()){
                 ROS_INFO("No arucos are deteted!!!");
+                image_pub_.publish(cv_ptr->toImageMsg());
             }else{
                 
             std::vector<cv::Vec3d> out_rvecs, out_tvecs, in_rvecs, in_tvecs;
 
 
             // 初始化消息
-            px_uav_msgs::TargetsInFrame aruco_land_msg;
+            uav_msgs::TargetsInFrame aruco_land_msg;
 
             // 查找外层和内层标记
             bool outerMarkerFound = false;
@@ -234,9 +235,14 @@ public:
             }
 
             // 发布机体系坐标消息
-            if (outerMarkerFound || innerMarkerFound || ids.size() != 0) {
+            if (outerMarkerFound || innerMarkerFound) {
                 aruco_land_pub_.publish(aruco_land_msg);
-                // 发布处理后的图像
+                
+            }else{
+                aruco_land_msg.targets.clear();
+                aruco_land_pub_.publish(aruco_land_msg);
+            }
+            // 发布处理后的图像
                 image_pub_.publish(cv_ptr->toImageMsg());
 
                 // 显示处理后的图像
@@ -244,8 +250,6 @@ public:
                 cv::resizeWindow("AR QR Detection Result", 800, 600);
                 cv::imshow("AR QR Detection Result", cv_ptr->image);
                 cv::waitKey(1); // 等待 1 毫秒，用于处理窗口事件
-            }
-            
             }
 
         } catch (cv_bridge::Exception& e) {
